4ac1e0ba74533242e1cc80b108855cd4ebed7835
ASSIMv3 <hassanassim01@gmail.com>
2024-01-03 00:44:29 +0100

    mandatory task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the specified number of rows.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
