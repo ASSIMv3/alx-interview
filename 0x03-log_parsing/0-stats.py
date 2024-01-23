8088d329880f862f564121365cb937b3ce528a23
ASSIMv3 <hassanassim01@gmail.com>
2024-01-23 08:18:42 +0100

    mandatory task  0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """reads from stdin and computes metrics"""
+import re
 import sys
 
-total_file_size = 0
+file_size = 0
 code = 0
 counter = 0
 dict_sc = {"200": 0, "301": 0, "400": 0, "401": 0,
@@ -10,32 +11,32 @@ dict_sc = {"200": 0, "301": 0, "400": 0, "401": 0,
            }
 
 
-def print_msg(dict_sc, total_file_size):
-    """Method to print"""
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def printCodes(dict, file_s):
+    """Prints the status code and the number of times they appear"""
+    print("File size: {}".format(file_s))
+    for key in sorted(dict.keys()):
+        if dict_sc[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
-
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])
-                code = parsed_line[1]
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                printCodes(dict_sc, file_size)
+            counter = counter + 1
+            try:
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                # print("Status Code {} size {}".format(statusC, f_size))
+                if statusC in dict_sc:
+                    dict_sc[statusC] += 1
+                file_size = file_size + f_size
+            except:
+                pass
+        printCodes(dict_sc, file_size)
+    except KeyboardInterrupt:
+        printCodes(dict_sc, file_size)
+        raise
