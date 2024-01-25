9f27021327da587adeac4583f3a87515d205304c
ASSIMv3 <hassanassim01@gmail.com>
2024-01-25 16:02:48 +0100

    mandatory task 0 fixing last checker

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,83 @@
 #!/usr/bin/python3
-""" Reads from stdin and computes metrics """
-
-import sys
+"""Reads from stdin and computes metrics"""
 import re
 
 
-def output(log: dict) -> None:
-    """ Display stats """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+def extract(log_line):
+    """Display stats"""
+    fields = (
+        r"\s*(?P<ip>\S+)\s*",
+        r"\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]",
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r"\s*(?P<status_code>\S+)",
+        r"\s*(?P<file_size>\d+)"
+    )
+
+    info = {
+        "status_code": 0,
+        "file_size": 0,
+    }
+
+    log_format = "{}\\-{}{}{}{}\\s*".format(fields[0],
+                                            fields[1],
+                                            fields[2],
+                                            fields[3],
+                                            fields[4])
+    resp_match = re.fullmatch(log_format, log_line)
+    if resp_match is not None:
+        status_code = resp_match.group("status_code")
+        file_size = int(resp_match.group("file_size"))
+        info["status_code"] = status_code
+        info["file_size"] = file_size
+    return info
+
+
+def print_stats(total_size, status_stats):
+    """Print the metrics"""
+    print("File size: {:d}".format(total_size), flush=True)
+    for status_code in sorted(status_stats.keys()):
+        num = status_stats.get(status_code, 0)
+        if num > 0:
+            print("{:s}: {:d}".format(status_code, num), flush=True)
+
+
+def update_metrics(line, total_size, status_stats):
+    """Updates the metrics"""
+    line_info = extract(line)
+    status_code = line_info.get("status_code", "0")
+    if status_code in status_stats.keys():
+        status_stats[status_code] += 1
+    return total_size + line_info["file_size"]
+
+
+def execute():
+    """Start the log parsing"""
+    line_num = 0
+    total_size = 0
+    status_stats = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_size = update_metrics(
+                line,
+                total_size,
+                status_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_stats(total_size, status_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_stats(total_size, status_stats)
 
 
 if __name__ == "__main__":
-    regex = re.compile(
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
-        r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] '
-        r'"GET /projects/260 HTTP/1.1" (.{3}) (\d+)'
-    )
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
-
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                log["file_size"] += file_size
-
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (line_count % 10 == 0):
-                    output(log)
-    finally:
-        output(log)
+    execute()
