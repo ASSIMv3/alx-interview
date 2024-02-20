dc2bd0a48126172c6ba7afa607bdda7a6bd609af
ASSIMv3 <hassanassim01@gmail.com>
2024-02-20 01:35:42 -0800

    fixing pycodestyle for task 0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,11 +6,12 @@ def rotate_2d_matrix(matrix):
     """Rotate a 2D matrix"""
     n = len(matrix)
     for i in range(n // 2):
-        last = n - 1 - i 
+        last = n - 1 - i
         for j in range(i, last):
             offset = j - i
             tmp = matrix[i][j]
-            matrix[i][j] = matrix[last - offset][i] # bottom to top
-            matrix[last - offset][i] = matrix[last][last - offset] # right to bottom
-            matrix[last][last - offset] = matrix[j][last] # top to right
-            matrix[j][last] = tmp # left to top
+            matrix[i][j] = matrix[last - offset][i]  # bottom to top
+            matrix[last - offset][i] = matrix[last][last - offset]
+            # right to bottom
+            matrix[last][last - offset] = matrix[j][last]  # top to right
+            matrix[j][last] = tmp  # left to top
