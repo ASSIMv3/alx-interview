6b2a9070c6ddadb842f26ece260f09e21a2537c2
ASSIMv3 <113550604+ASSIMv3@users.noreply.github.com>
2024-02-28 17:44:38 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""This is to make a change
-"""
+"""0-making_change Module"""
 
 
 def makeChange(coins, total):
-    """function with 2 arguments"""
-    if total <= 0:
+    """Return fewest number of coins needed to meet total"""
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
-
-    dp = [total + 1] * (total + 1)  # Initialize with total + 1
-    dp[0] = 0
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != total + 1 else -1
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+    return temp_value if total == 0 else -1
