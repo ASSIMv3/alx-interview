1cc0a62088794bf3ca5c386b05087a98711ddc76
ASSIMv3 <hassanassim01@gmail.com>
2024-03-04 11:48:59 +0100

    island perimeter interview

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function that returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """calculate the perimeter of the island described in the grid.
+    Args: The grid representing the island.
+    Returns: The perimeter of the island.
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
